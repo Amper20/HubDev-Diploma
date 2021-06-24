4e20f60b824bd0656ce24cd8ed348358ccbc76a3
Amper20 <butmalai.dan@gmail.com>
2021-06-25 01:03:24 +0300

    asd

--- sketch_feb09a/test_graph.py
+++ sketch_feb09a/test_graph.py
@@ -18,10 +18,10 @@ start = 0
 stop = 24
 step = 0.001
 
-change_period = 0.6
-cahnge_factor_a = change_period/3
-cahnge_factor_b = change_period/10
-day_factor_a = 0.4
+change_period = 1
+cahnge_factor_a = change_period/1
+cahnge_factor_b = change_period/100
+day_factor_a = 0.3
 day_factor_b = 0.6
 sunrise_factor_a = 0.5
 
@@ -40,20 +40,22 @@ power_max = 950
 
 for x in np.arange(start, stop, step):
     
-    x_axis.append(x)
+   
     
-    # generate sunrise
-    if(x >= sunrise_start and x <= sunrise_end):
-        pass
-        x_local = (x - sunrise_start) 
-        x_local_pow_a = change_period/2 - x_local
-        power_output_a.append((power_max-power_min) * (sunrise_factor_a) * math.pow(math.e, (-(x_local_pow_a*x_local_pow_a)/cahnge_factor_a)))
+    # # # generate sunrise
+    # if(x >= sunrise_start and x <= sunrise_end):
+    #     x_axis.append(x)
+    #     pass
+    #     x_local = (x - sunrise_start) 
+    #     x_local_pow_a = change_period/2 - x_local
+    #     power_output_a.append((power_max-power_min) * (sunrise_factor_a) * math.pow(math.e, (-(x_local_pow_a*x_local_pow_a)/cahnge_factor_a)))
         
-        x_local_pow_b = x_local - change_period
-        power_output_b.append((power_max-power_min) * day_factor_b * math.pow(math.e, (-(x_local_pow_b*x_local_pow_b)/cahnge_factor_b)))
+    #     x_local_pow_b = x_local - change_period
+    #     power_output_b.append((power_max-power_min) * day_factor_b * math.pow(math.e, (-(x_local_pow_b*x_local_pow_b)/cahnge_factor_b)))
         
     # generate sunset
-    elif(x >= sunset_start and x <= sunset_end):
+    if(x >= sunset_start and x <= sunset_end):
+        x_axis.append(x)
         pass
         x_local = (x - sunset_start) 
         x_local_pow_a = change_period/2 - x_local
@@ -62,20 +64,21 @@ for x in np.arange(start, stop, step):
         x_local_pow_b = x_local
         power_output_b.append((power_max-power_min) * day_factor_b * math.pow(math.e, (-(x_local_pow_b*x_local_pow_b)/cahnge_factor_b)))
 
-    # generate evning
-    elif(x >= sunrise_start and x <= sunset_end):
-        pass
-        power_output_a.append((power_max-power_min)*day_factor_a)
-        power_output_b.append((power_max-power_min)*day_factor_b)
-    # generate night
-    elif(True):
-        pass
-        power_output_a.append(power_min)
-        power_output_b.append(power_min)
+    # # generate evning
+    # elif(x >= sunrise_start and x <= sunset_end):
+    #     pass
+    #     power_output_a.append((power_max-power_min)*day_factor_a)
+    #     power_output_b.append((power_max-power_min)*day_factor_b)
+    # # generate night
+    # elif(True):
+    #     pass
+    #     power_output_a.append(power_min)
+    #     power_output_b.append(power_min)
 
 # # print(power_output_a)
 sum1 = [x+y for x, y in zip(power_output_a, power_output_b)]
-plt.plot(x_axis, power_output_a)
-plt.plot(x_axis, sum1)
-plt.plot(x_axis, power_output_b)
+# plt.plot(x_axis, sum1)
+plt.plot(x_axis, power_output_b, label="power_b")
+plt.plot(x_axis, power_output_a, label="power_a")
+plt.legend()
 plt.savefig('test_graph_computed.png')
