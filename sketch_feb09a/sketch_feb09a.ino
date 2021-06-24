463274a18e1c3b4f1aa73606d4d250aea1271a94
Amper20 <butmalai.dan@gmail.com>
2021-06-24 18:50:04 +0300

    asd

--- sketch_feb09a/sketch_feb09a.ino
+++ sketch_feb09a/sketch_feb09a.ino
@@ -5,21 +5,26 @@
 #include <ArduinoJson.h>
 
 //Do not modify
-int device_is_host = 0;
+int device_is_host = 1;
 WiFiServer server(80);
 WiFiClient wifiClient;
 
 
 //Configurable
-char dev_ssid[20] = "smart_light";
+char dev_ssid[20] = "smart-light";
 char dev_psswd[20] = "123321123";
+
 char wifi_ssid[20] = "horseman";
 char wifi_psswd[20] = "123321123";
+
+char id[25] = "None";
+
 char server_loc[30]= "http://192.168.0.101:5001/";
 
 // headers
 String http_request(char* url, char **params, int n);
 void get_power(double x, int out[2]);
+void parse_request(String s);
 
 // Device specific Setup
 int led_pin_hot = D5;
@@ -31,19 +36,20 @@ unsigned long time_now = 0;
 unsigned long curr_millis = 0;
 
 unsigned long time_update_server_prev_millis = 0;
-unsigned long time_update_server_rate = 60*60*1000; //1 hour
+unsigned long time_update_server_rate = 30*60*1000; // 30 min
 
 unsigned long time_update_local_prev_millis = 0;
 unsigned long time_update_local_rate = 1000; //1s
 
 unsigned long time_power_prev_millis = 0;
-unsigned long time_power_update_rate = 1*10*1000; //30 s
+unsigned long time_power_update_rate = 1*5*1000; //5 s
 
 
 void setup_(){
    pinMode(led_pin_hot, OUTPUT);
    pinMode(led_pin_cold, OUTPUT);
    pinMode(active_pin, INPUT);
+   ap_down();
    Serial.begin(115200);
 }
 
@@ -52,13 +58,14 @@ void work(){
     
     curr_millis = millis();
 
-    if(curr_millis - time_update_server_prev_millis >= time_update_server_rate || first_start) {
+    if((curr_millis - time_update_server_prev_millis >= time_update_server_rate || first_start) && !device_is_host) {
 
         time_update_server_prev_millis = curr_millis;
     
         char url[60];
         strcpy(url, server_loc);
         strcat(url, "get_seconds_from_midnight");
+        Serial.println(url);
         long time_from_server = http_request(url, NULL, 0).toInt();
         if(time_from_server >= 0)
             time_now = time_from_server;
@@ -70,18 +77,6 @@ void work(){
         Serial.println(time_now);
         double time_now_hour = ((double)time_now)/(60.0*60.0);
         int power[2] = {0, 0};
-        
-        /*
-            sim
-            for(int i = 0 ;i < 60*60*24; i+=60) {
-                get_power(i/(60.0*60.0), power);
-                Serial.print(i);
-                Serial.print(" ");
-                Serial.print(power[0]);
-                Serial.print(" ");
-                Serial.println(power[1]);
-            }
-        */
 
         get_power(time_now_hour, power);
         
@@ -103,11 +98,11 @@ void work(){
 }
 
 //power setup
-double power_min = 0, power_max = 500;
+double power_min = 0, power_max = 950;
 double e = 2.71828;
 
 double change_period = 0.5;
-double cahnge_factor = change_period/10, day_factor_a = 0.2, day_factor_b = 0.9;
+double cahnge_factor = change_period/10, day_factor_a = 0.5, day_factor_b = 0.5;
 
 double sunrise_end = 7, sunset_end = 23;
 double sunrise_start = sunrise_end - change_period, sunset_start = sunset_end - change_period;
@@ -141,6 +136,56 @@ void get_power(double x, int out[2]){
     }
 }
 
+typedef struct{
+    char key[15];
+    char type[10];
+    void *value;
+} param;
+
+void parse_request(String s){
+    
+    // modify for more parameters
+    int param_cnt = 9;
+    param x[10] = {
+        {"power_min", "double", &power_min},
+        {"power_max", "double", &power_max},
+        {"day_factor_a", "double", &day_factor_a},
+        {"day_factor_b", "double", &day_factor_b},
+        {"change_period", "double", &change_period},
+        {"sunrise_end", "double", &sunrise_end},
+        {"sunrise_start", "double", &sunrise_start},
+        {"dev_id", "string", id},
+        {"time_now", "ulong", &time_now},
+    };
+    if(strstr(s.c_str(), "setting") != NULL){
+        for (int i = 0 ; i < param_cnt; i++){
+            char *key = NULL;
+            if(key = strstr(s.c_str(), x[i].key)){ // Key matches
+                char value[10];
+                key += strlen(x[i].key) + 1; // skip key and '='
+                for(int j = 0; key && *key != '&' && *key != ' '; j++, key++, value[j] = '\0'){
+                    value[j] = *key;
+                }
+                if(strstr(x[i].type, "double") != NULL ){
+                    char *eptr;
+                    *(double *)(x[i].value) = strtod(value, &eptr);
+                }
+                if(strstr(x[i].type, "int") != NULL ){
+                    *(int *)(x[i].value) = atoi(value); 
+                }
+                if(strstr(x[i].type, "ulong") != NULL ){
+                    *(unsigned long *)(x[i].value) = atoi(value); 
+                }
+                if(strstr(x[i].type, "string") != NULL ){
+                    strcpy((char *)x[i].value, value);
+                }
+                Serial.print(x[i].key);
+                Serial.print(" ");
+                Serial.println(value);
+            }
+        }
+    }
+}
 
 String http_request(char *url, char **params, int n){
     
@@ -178,50 +223,51 @@ void setup() {
 void ap_active(){
     if (!device_is_host) {
         Serial.println("Setting soft-AP ... ");
-        device_is_host = WiFi.softAP(dev_ssid, dev_psswd) ? 1 : 0;
+        WiFi.mode(WIFI_AP_STA);
+        while(!device_is_host)
+            device_is_host = WiFi.softAP(dev_ssid, dev_psswd) ? 1 : 0;
+        server.begin();
     }
 }
 
 void ap_down(){
-    if(device_is_host)
-        device_is_host = WiFi.softAPdisconnect(true) ? 1 : 0;
-}
-
-void conn_up(){
-    if (WiFi.status() != WL_CONNECTED) {
-        WiFi.begin(wifi_ssid, wifi_psswd);
-        while (WiFi.status() != WL_CONNECTED) {
-            delay(1000);
-            Serial.print("...");
+    if(device_is_host) {
+        while(device_is_host)
+            device_is_host = WiFi.softAPdisconnect(true) ? 1 : 0;
+        WiFi.mode(WIFI_STA);
+        first_start = 1;
+        if (WiFi.status() != WL_CONNECTED) {
+            WiFi.begin(wifi_ssid, wifi_psswd);
+            while (WiFi.status() != WL_CONNECTED) {
+                delay(1000);
+                Serial.print("...");
+            }
+        } else{
+            Serial.println("connected to wifi");
         }
-    }
-}
-
-void conn_down(){
-    if (WiFi.status() == WL_CONNECTED) {
-       WiFi.mode(WIFI_OFF);
     }
 }
 
 void handle_server() {
 
     WiFiClient client = server.available();
-
+    Serial.println(WiFi.softAPIP());
+    delay(1000);
     if(!client) {
         return;
     }
     
     String request = client.readStringUntil('\r');
+    
     Serial.println(request);
     client.flush();
+    
     String value;
-    if(request.indexOf("/led/on") != -1) {
-        digitalWrite(led_pin_hot, HIGH);
-        value = "on";
-    } else if (request.indexOf("/led/off") != -1) {
-        digitalWrite(led_pin_hot, LOW);
-        value = "off";
+    
+    if(request.indexOf("/setting/") != -1) {
+        parse_request(request);
     }
+    
     client.flush();
 
     String s = "HTTP/1.1 200 OK\r\n";
@@ -234,6 +280,7 @@ void handle_server() {
     client.print(s);
     delay(1);
     Serial.println("Client disconnected");
+    client.stop();
 }
 
 bool device_is_active(){
@@ -244,12 +291,9 @@ void loop() {
 
   if (device_is_active()) {
         ap_down();
-        conn_up();
-        work();
   } else {
         ap_active();
-        conn_down();
         handle_server();
   }
-  
+  work();
 }
--- sketch_feb09a/test_graph.py
+++ sketch_feb09a/test_graph.py
@@ -2,75 +2,80 @@ import math
 import numpy as np 
 import matplotlib.pyplot as plt
 
-power_output_a = []
-power_output_b = []
-x_axis = []
-
-with open("sim.txt") as f:
-    lines = f.readlines()
-    lines = [ x.replace("\n", '').split(' ') for x in lines]
-    print(lines)
-    x_axis = [ (float(x[0])/(60*60)) for x in lines ]
-    power_output_a = [ float(x[1]) for x in lines ]
-    power_output_b = [ float(x[2]) for x in lines ]
-# start = 0
-# stop = 24
-# step = 0.001
-
-# change_period = 0.5
-# cahnge_factor = change_period/10
-# day_factor_a = 0.2
-# day_factor_b = 0.9
-
-# sunrise_end = 7
-# sunrise_start = sunrise_end - change_period
-
-# sunset_end = 23
-# sunset_start = sunset_end - change_period
-
 # power_output_a = []
 # power_output_b = []
 # x_axis = []
 
-# power_min = 0
-# power_max = 1023
-
-# for x in np.arange(start, stop, step):
+# with open("sim.txt") as f:
+#     lines = f.readlines()
+#     lines = [ x.replace("\n", '').split(' ') for x in lines]
+#     print(lines)
+#     x_axis = [ (float(x[0])/(60*60)) for x in lines ]
+#     power_output_a = [ float(x[1]) for x in lines ]
+#     power_output_b = [ float(x[2]) for x in lines ]
     
-#     x_axis.append(x)
-    
-#     # generate sunrise
-#     if(x >= sunrise_start and x <= sunrise_end):
-#         pass
-#         x_local = (x - sunrise_start) 
-#         x_local_pow_a = change_period/2 - x_local
-#         power_output_a.append((power_max-power_min) * math.pow(math.e, (-(x_local_pow_a*x_local_pow_a)/cahnge_factor)))
-        
-#         x_local_pow_b = x_local - change_period
-#         power_output_b.append((power_max-power_min) * day_factor_b * math.pow(math.e, (-(x_local_pow_b*x_local_pow_b)/cahnge_factor)))
-        
-#     # generate sunset
-#     elif(x >= sunset_start and x <= sunset_end):
-#         pass
-#         x_local = (x - sunset_start) 
-#         x_local_pow_a = change_period/2 - x_local
-#         power_output_a.append((power_max-power_min) * math.pow(math.e, (-(x_local_pow_a*x_local_pow_a)/cahnge_factor)))
-        
-#         x_local_pow_b = x_local
-#         power_output_b.append((power_max-power_min) * day_factor_b * math.pow(math.e, (-(x_local_pow_b*x_local_pow_b)/cahnge_factor)))
+start = 0
+stop = 24
+step = 0.001
 
-#     # generate evning
-#     elif(x >= sunrise_start and x <= sunset_end):
-#         pass
-#         power_output_a.append((power_max-power_min)*day_factor_a)
-#         power_output_b.append((power_max-power_min)*day_factor_b)
-#     # generate night
-#     elif(True):
-#         pass
-#         power_output_a.append(power_min)
-#         power_output_b.append(power_min)
+change_period = 0.6
+cahnge_factor_a = change_period/3
+cahnge_factor_b = change_period/10
+day_factor_a = 0.4
+day_factor_b = 0.6
+sunrise_factor_a = 0.5
+
+sunrise_end = 7
+sunrise_start = sunrise_end - change_period
+
+sunset_end = 23
+sunset_start = sunset_end - change_period
+
+power_output_a = []
+power_output_b = []
+x_axis = []
+
+power_min = 0
+power_max = 950
+
+for x in np.arange(start, stop, step):
+    
+    x_axis.append(x)
+    
+    # generate sunrise
+    if(x >= sunrise_start and x <= sunrise_end):
+        pass
+        x_local = (x - sunrise_start) 
+        x_local_pow_a = change_period/2 - x_local
+        power_output_a.append((power_max-power_min) * (sunrise_factor_a) * math.pow(math.e, (-(x_local_pow_a*x_local_pow_a)/cahnge_factor_a)))
+        
+        x_local_pow_b = x_local - change_period
+        power_output_b.append((power_max-power_min) * day_factor_b * math.pow(math.e, (-(x_local_pow_b*x_local_pow_b)/cahnge_factor_b)))
+        
+    # generate sunset
+    elif(x >= sunset_start and x <= sunset_end):
+        pass
+        x_local = (x - sunset_start) 
+        x_local_pow_a = change_period/2 - x_local
+        power_output_a.append((power_max-power_min) * (sunrise_factor_a) * math.pow(math.e, (-(x_local_pow_a*x_local_pow_a)/cahnge_factor_a)))
+        
+        x_local_pow_b = x_local
+        power_output_b.append((power_max-power_min) * day_factor_b * math.pow(math.e, (-(x_local_pow_b*x_local_pow_b)/cahnge_factor_b)))
+
+    # generate evning
+    elif(x >= sunrise_start and x <= sunset_end):
+        pass
+        power_output_a.append((power_max-power_min)*day_factor_a)
+        power_output_b.append((power_max-power_min)*day_factor_b)
+    # generate night
+    elif(True):
+        pass
+        power_output_a.append(power_min)
+        power_output_b.append(power_min)
 
 # # print(power_output_a)
+sum1 = [x+y for x, y in zip(power_output_a, power_output_b)]
 plt.plot(x_axis, power_output_a)
+plt.plot(x_axis, sum1)
 plt.plot(x_axis, power_output_b)
 plt.savefig('test_graph_computed.png')
